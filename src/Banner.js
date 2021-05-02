24549627c07621f8a5ec0018a7f122810fbbf76d
MitulPatel5522 <42907640+MitulPatel5522@users.noreply.github.com>
2021-05-02 17:58:43 +0530

    Removed Unused imports

--- src/Banner.js
+++ src/Banner.js
@@ -1,12 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Banner.css";
-import { Button } from "@material-ui/core";
-import Search from "./Search";
-import { useHistory } from "react-router-dom";
 
 function Banner() {
-  const history = useHistory();
-  const [showSearch, setShowSearch] = useState(false);
 
   return <div className="banner"></div>;
 }
